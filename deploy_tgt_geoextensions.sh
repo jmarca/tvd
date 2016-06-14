e0a10d1eeee4b62d50e099dfe22a0b42dcf223d6
James E. Marca <james@activimetrics.com>
2016-06-14 14:16:01 -0700

    reworking how to sqitch deploy via npm
    use sqitch deploy inside of a run script, call down to it from local.
    
    Unfortunately this presumes that I can find the dependency exactly at
    node_modules/<dependency> which is dumb and not future proof.

--- deploy_tgt_geoextensions.sh
+++ deploy_tgt_geoextensions.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 
 tgt_db=${npm_package_config_tgt_db:-"osm"}
 tgt_host=${npm_package_config_tgt_host:-"127.0.0.1"}
@@ -9,4 +8,7 @@ tgt_user=${npm_package_config_tgt_user:-""}
 
 SQITCH_TGT_DB_URI=${SQITCH_DB_URI:-"db:pg://${tgt_user}@${tgt_host}:${tgt_port}/$tgt_db"}
 
-SQITCH_DB_URI=${SQITCH_TGT_DB_URI} npm install ../calvad_db_geoextensions
+
+cd node_modules/calvad_db_geoextensions/
+
+SQITCH_DB_URI=${SQITCH_TGT_DB_URI} npm run sqitch:deploy
