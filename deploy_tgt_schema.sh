a4041fe27f8682f87a8d7912350b531cfe0c275b
James E. Marca <james@activimetrics.com>
2016-06-13 18:36:33 -0700

    ugh
    recursive hell because npm doesn't have wrapping lifecycle stages for
    npm install

---
+++ deploy_tgt_schema.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -x
+
+tgt_db=${npm_package_config_tgt_db:-"osm"}
+tgt_host=${npm_package_config_tgt_host:-"127.0.0.1"}
+tgt_port=${npm_package_config_tgt_port:-5432}
+tgt_user=${npm_package_config_tgt_user:-""}
+
+
+SQITCH_TGT_DB_URI=${SQITCH_DB_URI:-"db:pg://${tgt_user}@${tgt_host}:${tgt_port}/$tgt_db"}
+
+SQITCH_DB_URI=${SQITCH_TGT_DB_URI} npm install jmarca/calvad_detectors_schema
